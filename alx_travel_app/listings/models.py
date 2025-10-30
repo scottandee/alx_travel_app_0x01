a2d5ad6e855a3b0922829b4fc91f4c8e1d1da144
Andee Olayinkascott <andeeolayinkascott@gmail.com>
2025-10-30 23:55:06 +0100

    Update models

--- alx_travel_app/listings/models.py
+++ alx_travel_app/listings/models.py
@@ -22,7 +22,7 @@ class User(AbstractUser):
 
     
 class Listing(models.Model):
-    property_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    listing_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     host_id = models.ForeignKey(User, on_delete=models.CASCADE, related_name="listings")
     name = models.CharField(max_length=200)
     description = models.TextField()
@@ -45,7 +45,7 @@ class Booking(models.Model):
     start_date = models.DateField()
     end_date = models.DateField()
     total_price = models.DecimalField(max_digits=10, decimal_places=2)
-    status = models.CharField(max_length=20, choices=BookingStatus.choices)
+    status = models.CharField(max_length=20, choices=BookingStatus.choices, default=BookingStatus.PENDING)
     created_at = models.DateTimeField(auto_now_add=True)
 
 
